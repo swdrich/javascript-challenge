82959466adcbb480d802860bc9ba3498ad24af00
Steph <swdrinkard@gmail.com>
2021-04-08 16:10:13 -0500

    Filter function working correctly and printing to the console.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,16 +2,52 @@ console.log("app.js is loaded");
 
 // from data.js
 var tableData = data;
-console.log(data);
+console.log(tableData);
 
 // Create reference to table in index page
 var tbody = d3.select("tbody");
 
 // Loop through data and add to rows
-data.forEach((ufoSighting) => {
+tableData.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
     });
 });
+
+// Create event handler to search for date
+// Select form
+var form = d3.select("#form");
+
+// Select button
+var button = d3.select("#filter-btn");
+
+// Create event handlers
+form.on("submit", runEnter);
+button.on("click", runEnter);
+
+// Build out function
+function runEnter() {
+
+    // Prevent refresh
+    d3.event.preventDefault();
+
+    // Select input element
+    var inputElement = d3.select("#datetime");
+
+    // Get value
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    // Filter
+    var filteredByDate = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredByDate);
+    return filteredByDate;
+
+    // Clear table
+
+
+};
+
+
